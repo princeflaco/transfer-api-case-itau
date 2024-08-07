6a6684a6284f67e6631000e0c307006cbfb37acd
princeflaco <leobbispo@hotmail.com>
2024-08-07 15:25:45 -0300

    adding concurrency control to deposits and withdraws

--- core/domain/account.go
+++ core/domain/account.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"github.com/google/uuid"
+	"sync"
 	"transfer-api/core/errors"
 )
 
@@ -9,6 +10,7 @@ type Account struct {
 	Id      string
 	UserId  string
 	Balance int
+	mu      sync.Mutex
 }
 
 func NewAccount(id string, userId string, balance int) *Account {
@@ -23,10 +25,14 @@ func NewAccount(id string, userId string, balance int) *Account {
 }
 
 func (a *Account) Deposit(amount int) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.Balance += amount
 }
 
 func (a *Account) Withdraw(amount int) error {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	if amount < a.Balance {
 		missingAmount := amount - a.Balance
 		return errors.NewInsufficientFundsError(a.Id, missingAmount)
